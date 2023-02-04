ce1b5cd51643a9a2e399a02bad2449a5c9422bb1
Anmol8604 <anmol.12018604@lpu.in>
2023-02-05 03:52:46 +0530

    Matrix Question Added

--- Matrix/SpiralMatrix.java
+++ Matrix/SpiralMatrix.java
@@ -27,7 +27,7 @@ import java.util.List;
 
 public class SpiralMatrix {
      public static void main(String[] args) {
-          int[][] arr = {{1}, {2}, {3}};//{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+          int[][] arr = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
           List<Integer> list = new ArrayList<>();
           list = spiral(arr, list);
 
