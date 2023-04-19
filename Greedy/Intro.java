d62a86a322bd672b833a6942b14395698281d492
Anmol8604 <anmol.12018604@lpu.in>
2023-04-19 23:02:32 +0530

    ll del

--- Greedy/Intro.java
+++ Greedy/Intro.java
@@ -5,14 +5,12 @@ public class Intro {
         // Greedy Algorithms are mainly used for optimizing problems.
         // What is an optimizing problems ?
         // Optimizing problems are maximizing or minimizing something. e.g-> Shortest path(In this problem we are finding minimum distance path or minimum weight path.), Longest path
-
         // Example :
         int[] coins = {10, 5, 2, 1};
         int amount = 52;
         // Task : Minimum no. of coins are required for certain amount.
         int requiredCoin = minimumNo_of_Coins(coins, amount);
         System.out.println("Minimum no. of coins : " + requiredCoin); // 6 (5 coins of rupees 10, 1 coin of rupees 2)
-
     }
 
     static int minimumNo_of_Coins(int[] coins, int amount) {
---
+++ Linked_List/LL.java
@@ -0,0 +1,5 @@
+package Linked_List;
+
+public class LinkedList {
+    
+}
